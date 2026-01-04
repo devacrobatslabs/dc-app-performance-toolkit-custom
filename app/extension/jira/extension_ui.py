cfa62e8700e2e58225790506890108e7ac7ecb3a
savovski80 <operations@narasyst.com>
2026-01-04 22:03:32 +0200

    STAC-1646 dcrecert

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -228,7 +228,7 @@ def app_add_remove_work_calendar(jira_webdriver, datasets):
         try:
             page.wait_until_visible((By.ID, 'login-form'))
             page.get_element((By.CSS_SELECTOR, 'input[name="webSudoPassword"]')).send_keys(JIRA_SETTINGS.admin_password)
-            page.get_element((By.ID, 'login-form-submit')).click()
+            page.wait_until_clickable((By.ID, 'login-form-submit')).click()
         except:
             pass
         # add calendar
@@ -266,7 +266,7 @@ def app_change_color_palette(jira_webdriver, datasets):
             page.wait_until_visible((By.ID, 'login-form'))
             # page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JIRA_SETTINGS.admin_password)
             page.get_element((By.CSS_SELECTOR, 'input[name="webSudoPassword"]')).send_keys(JIRA_SETTINGS.admin_password)
-            page.get_element((By.ID, 'login-form-submit')).click()
+            page.wait_until_clickable((By.ID, 'login-form-submit')).click()
         except:
             pass
         page.wait_until_visible((By.CSS_SELECTOR, '.po-page'))
