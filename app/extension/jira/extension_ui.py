52c3b3847b35e41fce7ebc4b757c998050febc9f
savovski80 <operations@narasyst.com>
2026-01-04 21:03:54 +0200

    STAC-1646 dcrecert

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -227,7 +227,7 @@ def app_add_remove_work_calendar(jira_webdriver, datasets):
 
         try:
             page.wait_until_visible((By.ID, 'login-form'))
-            page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JIRA_SETTINGS.admin_password)
+            page.get_element((By.CSS_SELECTOR, 'input[name="webSudoPassword"]')).send_keys(JIRA_SETTINGS.admin_password)
             page.get_element((By.ID, 'login-form-submit')).click()
         except:
             pass
@@ -264,7 +264,8 @@ def app_change_color_palette(jira_webdriver, datasets):
 
         try:
             page.wait_until_visible((By.ID, 'login-form'))
-            page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JIRA_SETTINGS.admin_password)
+            # page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JIRA_SETTINGS.admin_password)
+            page.get_element((By.CSS_SELECTOR, 'input[name="webSudoPassword"]')).send_keys(JIRA_SETTINGS.admin_password)
             page.get_element((By.ID, 'login-form-submit')).click()
         except:
             pass
--- app/extension/jsm/extension_ui_agents.py
+++ app/extension/jsm/extension_ui_agents.py
@@ -228,7 +228,8 @@ def app_add_remove_work_calendar(jira_webdriver, datasets):
 
         try:
             page.wait_until_visible((By.ID, 'login-form'))
-            page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JSM_SETTINGS.admin_password)
+            # page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JSM_SETTINGS.admin_password)
+            page.get_element((By.CSS_SELECTOR, 'input[name="webSudoPassword"]')).send_keys(JSM_SETTINGS.admin_password)
             page.get_element((By.ID, 'login-form-submit')).click()
         except:
             pass
@@ -265,7 +266,8 @@ def app_change_color_palette(jira_webdriver, datasets):
 
         try:
             page.wait_until_visible((By.ID, 'login-form'))
-            page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JSM_SETTINGS.admin_password)
+            # page.get_element((By.ID, 'login-form-authenticatePassword')).send_keys(JSM_SETTINGS.admin_password)
+            page.get_element((By.CSS_SELECTOR, 'input[name="webSudoPassword"]')).send_keys(JSM_SETTINGS.admin_password)
             page.get_element((By.ID, 'login-form-submit')).click()
         except:
             pass
