850a8277407f68542883bd73aae648e4e631b513
savovski80 <operations@narasyst.com>
2026-01-04 11:56:38 +0200

    STAC-1646 merged

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -295,6 +295,15 @@ def close_info_popups(page):
     
 
     try:
+        clickList = page.get_elements((By.CSS_SELECTOR, '.jira-help-tip button'))
+        if clickList:
+            for clickEl in clickList:
+                clickEl.click()
+            page.wait_until_invisible((By.CSS_SELECTOR, '.jira-help-tip'))
+    except:
+        pass
+
+    try:
         clickList = page.get_elements((By.CSS_SELECTOR, '.insiders-signup-form .cancel'))
         if clickList:   # empty array is false in python
             for clickEl in clickList:
@@ -303,14 +312,6 @@ def close_info_popups(page):
     except:
         pass
 
-    try:
-        clickList = page.get_elements((By.CSS_SELECTOR, '.jira-help-tip button'))
-        if clickList:
-            for clickEl in clickList:
-                clickEl.click()
-            page.wait_until_invisible((By.CSS_SELECTOR, '.jira-help-tip'))
-    except:
-        pass
 
     try:
         clickList = page.get_elements((By.CSS_SELECTOR, '.jira-help-tip .helptip-close'))
